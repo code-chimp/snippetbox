e2e7b4ac48dbf27bd404fbdf8852891eab30e4ce
Tim Goshinski <tim@code-chimp.com>
2024-03-31 08:30:24 -0500

    :sparkles: Ch 6 Middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
@@ -12,5 +15,7 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /snippet/create", app.getSnippetForm)
 	mux.HandleFunc("POST /snippet/create", app.postSnippetForm)
 
-	return mux
+	baseMiddlewares := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return baseMiddlewares.Then(mux)
 }
